11c9106e8cfd693504fae279abb75b18cb8e1296
Anastasia Buzanova <ms.staisy@gmail.com>
2019-12-08 01:39:43 +0300

    Animate bug  fixed

--- TopNews/NewsModule/View/NewsViewController.swift
+++ TopNews/NewsModule/View/NewsViewController.swift
@@ -91,50 +91,50 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: true)
-
+        
         //__________Анимация_________
         let figure1  = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 60))
         figure1.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure1.center = CGPoint(x: view.center.x - 13, y: view.center.y - 28)
         view.addSubview(figure1)
-
+        
         let figure22  = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 10))
         figure22.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure22.center = CGPoint(x: view.center.x + 13, y: view.center.y - 25)
         view.addSubview(figure22)
-
+        
         let figure2  = UIView(frame: CGRect(x: 0, y: 0, width: 60, height: 10))
         figure2.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure2.center = CGPoint(x: view.center.x + 28, y: view.center.y - 13)
         view.addSubview(figure2)
-
+        
         let figure33  = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 30))
         figure33.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure33.center = CGPoint(x: view.center.x + 25, y: view.center.y + 13)
         view.addSubview(figure33)
-
+        
         let figure3  = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 60))
         figure3.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure3.center = CGPoint(x: view.center.x + 13, y: view.center.y + 28)
         view.addSubview(figure3)
-
+        
         let figure44  = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: 10))
         figure44.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure44.center = CGPoint(x: view.center.x - 13, y: view.center.y + 25)
         view.addSubview(figure44)
-
+        
         let figure4  = UIView(frame: CGRect(x: 0, y: 0, width: 60, height: 10))
         figure4.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure4.center = CGPoint(x: view.center.x - 28, y: view.center.y + 13)
         view.addSubview(figure4)
-
+        
         let figure11  = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 30))
         figure11.backgroundColor = UIColor(red:0.10, green:0.4, blue:0.35, alpha: 1.0)
         figure11.center = CGPoint(x: view.center.x - 25, y: view.center.y - 13)
         view.addSubview(figure11)
-
+        
         var counter = 0
-
+        
         print(counter)
         for item in view.subviews{
             UIView.animate(withDuration: 0.5, delay: 0.1 * TimeInterval(counter), options: [.repeat, .autoreverse, .beginFromCurrentState]                  , animations: {
@@ -159,7 +159,7 @@ class NewsViewController: UIViewController, UITableViewDataSource, UITableViewDe
     func refresh(){
         refreshButton.isHidden = true
         label.isHidden = true
-
+        
         print("--------StartRefreshing-------")
         refreshControl.endRefreshing()
         presenter.downloadNews(country: "us", category: "entertainment", token: "3ea77deb1c4447eb8dd3619b369fb042")
@@ -175,6 +175,8 @@ extension NewsViewController: NewsViewProtocol{
     func success() {
         
         DispatchQueue.main.async {
+            UIView.setAnimationsEnabled(false)
+            self.view.layer.removeAllAnimations()
             self.view.addSubview(self.tableView)
             self.tableView.refreshControl = self.refreshControl
             self.tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.reuseID)
